2e9b5cf8598cdf7307ebde197f6e706e2643e835
Willio06 <tuur.willio@gmail.com>
2025-06-03 00:18:36 +0200

    done

--- Assignment1/ReducedOrder.py
+++ Assignment1/ReducedOrder.py
@@ -1,49 +1,53 @@
-from FiniteElements import FiniteElements_1D
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
-
+import cupy as cp
+import time
 N = 10**3
 
 class ReducedOrder:
-    def __init__(self, mus,MU, boundary=0, added_boundary=False):
-        self.mus = mus
-        self.n = len(mus)
-        self.MU = MU
-        self.boundary = boundary
-        self.added_boundary = added_boundary
+    def __init__(self, mus,MU, boundary=0):
+        self.mus = mus # mu_i's
+        self.n = len(mus) 
+        self.MU = MU # the wanted mu
+        self.boundary = boundary # adds zero boundary condition at the end if wanted
         self.__MatrixAssembly()
     def __FEMinit(self, mu, decompose=False, FEMsolve=False):
+        """
+        Initialize the finite element method matrices based on the given mu.
+        Solves if wanted, and returns the matrices A and b."""
         h = 1/N
-        B1 = (1/h)*(np.diag((N-1)*[2]) + np.diag((N-2)*[-1], 1)+ np.diag((N-2)*[-1], -1))
-        B2 = (h)*(np.diag((N-1)*[2/3]) + np.diag((N-2)*[1/6], 1)+ np.diag((N-2)*[1/6], -1))
-        B3 = np.diag((N-2)*[1/2], 1)+ np.diag((N-2)*[-1/2], -1)
+        B1 = (1/h)*(cp.diag(cp.full((N-1,), 2)) + cp.diag(cp.full((N-2,), -1), 1)+ cp.diag(cp.full((N-2,), -1), -1))
+        B2 = (h)*(cp.diag(cp.full((N-1,), 2/3)) + cp.diag(cp.full((N-2,), 1/6), 1)+ cp.diag(cp.full((N-2,), 1/6), -1))
+        B3 = cp.diag(cp.full((N-2,), 1/2), 1)+ cp.diag(cp.full((N-2,), -1/2), -1)
         A = mu[0]*B1 + mu[1]*B2 + mu[2]*B3
-        b = h*np.ones(N-1)
+        b = h*cp.ones(N-1)
+
+        if FEMsolve:
+            x = cp.linalg.solve(A, b)
+            self.FEMx = x
         if decompose:
             return mu[0]*B1, mu[1]*B2, mu[2]*B3,b
-        x = np.linalg.solve(A, b)
-        if FEMsolve:
-            if not self.added_boundary:
-                x = np.append(x, self.boundary)
-                x = np.insert(x, 0,self.boundary)
-            self.FEMx = x
         return A, b
     def __FEMspan(self):
-        C = np.zeros((N-1,self.n))
-        for mu in self.mus:
-            A, b = self.__FEMinit(mu)
-            fem = FiniteElements_1D(0, 1, A, b, boundary=0, added_boundary=True)
-            x = fem.solve()
-            C[:, self.mus.index(mu)] = x
+        """
+        Create a matrix C that spans the finite element space for the given mu_i's."""
+        C = cp.zeros((N-1,self.n))
+        for i in range(len(self.mus)):
+            mu = self.mus[i]
+            A, b = self.__FEMinit(mu, FEMsolve=True) #solve the FEM problem for each mu_i
+            x = self.FEMx
+            C[:, i] = x
         return C
     def __MatrixAssembly(self):
+        """
+        Assemble the matrices A, B1, B2, B3 and b for the reduced order model."""
         C = self.__FEMspan()
-        B1,B2,B3,b = self.__FEMinit(self.MU, decompose=True, FEMsolve=True)
-        B1 = np.transpose(C)*B1*C
-        B2 = np.transpose(C)*B2*C
-        B3 = np.transpose(C)*B3*C
-        b = np.transpose(C)*b
+        self.C = C
+        B1,B2,B3,b = self.__FEMinit(self.MU, decompose=True, FEMsolve=True) # note that the last femx is this one and thus for the wanted mu
+        B1 = cp.transpose(C)@B1@C
+        B2 = cp.transpose(C)@B2@C
+        B3 = cp.transpose(C)@B3@C
+        b = cp.transpose(C)@b
         A = B1 + B2 + B3
         self.A = A
         self.C = C
@@ -52,38 +56,23 @@ class ReducedOrder:
         self.B2 = B2
         self.B3 = B3
     def solve(self):
-        x = np.linalg.solve(self.A, self.b)
-        if not self.added_boundary:
-            x = np.append(x, self.boundary)
-            x = np.insert(x, 0,self.boundary)
+        x = cp.linalg.solve(self.A, self.b)
         self.x = x
         return x
-    def plotter(self, x, y=None, title=None, xlabel=None, ylabel=None, logscale=False, labelx=None, labely=None):
-        size_x  = np.shape(x)[0]
+    def addBoundary(self, x: cp.ndarray):
+        """Add a boundary condition to the solution."""
+        # will be useless because the boundary condition is not approximated and thus will always have zero error value for any approximation with
+        # added boundary condition, thus it is equivalent to just leaving it out
+        x = cp.append(x, self.boundary)
+        x = cp.concatenate((cp.array([self.boundary]),x))
+        return x
+        
 
-        x_step = np.arange(self.start, self.end, (self.end-self.start)/size_x)
-
-        plt.plot(x_step, x, drawstyle='default', color='blue', linewidth=2,label=labelx)
-        plt.xlim(self.start, self.end)
-        # plt.ylim(0, max(self.x) * 1.1)
-       
-        if y is not None:
-            size_y  = np.shape(y)[0]
-            y_step = np.arange(self.start, self.end, (self.end-self.start)/size_y)
-            plt.plot(y_step, y, drawstyle='default', color='red', linewidth=2,label=labelx)
-            plt.legend()
-
-
-        plt.xlabel(xlabel if xlabel else 'X axis')
-        plt.ylabel(ylabel if ylabel else 'Y axis')
-        if logscale:
-            plt.yscale('log')
-        else:
-            plt.yscale('linear')
-        plt.title(title if title else 'Plot title')
-        plt.grid(True, linestyle='--', alpha=0.5)
 class P:
-    def __init__(self, n=None, alternative=False):
+    """ class to represent and generate points in the parameter space P and P_hat
+        alternative=True means that the points are generated in the alternative way
+    """
+    def __init__(self, n=None, alternative=False): 
         self.n = int(np.cbrt(n))
         self.x_range = (1e-1, 1.0)
         self.y_range = (0.0, 1.0)
@@ -92,6 +81,7 @@ class P:
         self.alternative = alternative
 
     def __iter__(self):
+        """Iterate over the points in the parameter space."""
         xs = np.linspace(*self.x_range, self.n)
         ys = np.linspace(*self.y_range, self.n)
         zs = np.linspace(*self.z_range, self.n)
@@ -107,40 +97,29 @@ class P:
             for point in grid:
                 yield point
     def random(self, size=1):
+        """Generate random points in the parameter space."""
         x = np.random.uniform(*self.x_range, size=size)
         y = np.random.uniform(*self.y_range, size=size)
         z = np.random.uniform(*self.z_range, size=size)
         return np.stack((x, y, z), axis=-1)
 
-def Vrom(greedy=True, n=500):
+def Vrom(p,greedy=True, n=500):
+    """ construction of Vrom by picking mu_i in random or greedy way"""
     if(not greedy):
         return P(10).random(size=n)
     else:
-        p = P(10).random(size=500)
-        selected = []
-        for i in range(n):
-            norms = []
-            for k in range(500):
-                if i==0:
-                    rb = ReducedOrder(p[k], p[k], boundary=0, added_boundary=False)
-                else:
-                    rb = ReducedOrder(selected, p[k], boundary=0, added_boundary=False)
-                x = rb.solve()
-                xfem = rb.FEMx
-                norms.append(H2_norm_approx(x-xfem, h=1/N))
-            i = np.argmax(norms)
-            selected.append(p[i])
+        """ this is just the initialization of  the greedy algo. To add greedly points, use the error function with greed=True"""
+        Mnorm = cp.array([]) # array to store the H2 norms of the FEM solutions
+        for mu in p:
+            femmer = ReducedOrder([mu], mu, boundary=0)# take for mus just mu, calculations will go faster with less mu, is not relevant here anyway just the FEM solution is needed
+            x = femmer.FEMx
+            Mnorm = cp.append(Mnorm,H2_norm_approx(x, h=1/N)) # compute the H2 norm of the FEM solution
+        selected = [p[cp.argmax(Mnorm).get()]] # start with the point with the highest H2 norm
         return selected
+
 def mixed_difference(x,h=1):
-    x = np.asarray(x)
-    if x.ndim != 1:
-        raise ValueError("Input must be a 1D array")
-
-    n = len(x)
-    if n < 2:
-        raise ValueError("Array must contain at least two elements")
-
-    diffs = np.empty_like(x)
+    """ use finite difference method to compute derivative of array for H1 norm approximation"""
+    diffs = cp.empty_like(x)
     diffs[:-1] = x[1:] - x[:-1]  # forward differences
     diffs[-1] = x[-1] - x[-2]    # backward difference for last element
     return diffs/h
@@ -148,43 +127,55 @@ def H2_norm_approx(x,h=1):
     """
     Use trapezoidal rule to approximate the H2 norm of a 1D array.
     """
-    x = np.abs(np.asarray(x))
-    if x.ndim != 1:
-        raise ValueError("Input must be a 1D array")
-
-    n = len(x)
-    if n < 2:
-        raise ValueError("Array must contain at least two elements")
-
+    x = cp.abs(x) # note absolute value
     diffs = mixed_difference(x, h)
     # Use trapezoidal rule for numerical integration
-    integral1 = np.trapz(diffs**2, dx=h)
-    integral2 = np.trapz(x**2, dx=h)
-    return np.sqrt(integral1 + integral2)
-def error(mus):
-    listy=[]
-    for mu in P(10).random(size=500):
+    integral1 = cp.trapz(diffs**2, dx=h)
+    integral2 = cp.trapz(x**2, dx=h)
+    return cp.asnumpy(cp.sqrt(integral1 + integral2))
+def error(mus,P, greed=False):
+    listy=cp.array([]) # list to store the errors
+    for mu in P: # iterate over all mu's that are not in mus
+        # print(" Computing error for mu:", mu, " at index", len(listy))
         mu = mu.tolist()
-        rb = ReducedOrder(mus,mu, boundary=0, added_boundary=True)
+        rb = ReducedOrder(mus,mu, boundary=0)
         x= rb.solve()
         xfem = rb.FEMx
-        listy.apppend(H2_norm_approx(x-xfem, h=1/N))
-    return np.max(listy), listy
+        cc = np.full(len(x), (N-1)//len(x), dtype=int) #extend x to length N
+        cc[: (N-1) % len(x)] += 1  # Adjust the last few elements to ensure correct length
+        x = cp.asarray(cp.repeat(cp.asnumpy(x), cc))
+        listy = cp.append(listy,H2_norm_approx(x-xfem, h=1/N))
+    if greed:
+        diff = [mu for mu in P if not np.any(np.all(mus == mu, axis=1))]#set difference between P and mus
+        mus.append(diff[cp.argmax(listy[np.all(~np.isin(P,mus), axis = 1)]).get()]) # take greedily max without duplicates, so in set difference
+        return cp.max(listy), listy, mus
+    return cp.max(listy), listy
 
 
 
 def ex3():
     error1=[]
+    t = time.time()
+    p =  P(10).random(size=500)
     for n in range(2,51):
-        Vrom1 = Vrom(greedy=False, n=n)
-        error1.append(error(Vrom1)[0])
+        print("Computing non-greedy ROM for n =", n, " time elapsed:", time.time() - t)
+        t = time.time()
+        Vrom1 = Vrom(p,greedy=False, n=n)
+        error1.append(error(Vrom1,p,greed=False)[0].get())
     error2=[]
+    
+    Vrom1 = Vrom(p,greedy=True, n=1)
+    _,_, Vrom1 = error(Vrom1, p, greed=True) # initialize with the first point
     for n in range(2,51):
-        Vrom1 = Vrom(greedy=True, n=n)
-        error2.append(error(Vrom1)[0])
+        print("Computing greedy ROM for n =", n, " time elapsed:", time.time() - t)
+        t = time.time()
+        err,_, Vrom1 = error(Vrom1, p, greed=True) # points are added greedily here already
+        error2.append(err.get())
     plt.plot(range(2,51), error1, label="Non-Greedy ROM")
     plt.plot(range(2,51), error2, label="Greedy ROM")
     plt.xlabel("Number of basis functions")
+    print(error1)
+    print(error2)
     plt.ylabel("Argmax H2 norm error")
     plt.title("Comparison of Greedy and Non-Greedy ROM")
     plt.yscale('log')
